19dd5de9e64f864f96fdb3a06c81accdab56f649
Jason Walker <walkerjason@live.com>
2016-11-14 14:01:45 -0500

    Update with UTF-8

--- Misc/Generate_SecurityTemplate.ps1
+++ Misc/Generate_SecurityTemplate.ps1
@@ -1,4 +1,5 @@
-﻿$properties = @()
+
+$properties = @()
 $properties += New-xDscResourceProperty -Name Path -Type String -Attribute Key -Description "Path to Inf the defines the desired security policies"
 
 $securityTemplateParameters = @{
