b3dbc53218d2d6cb9a14ee5dd4e31359fbdcc780
Jason Walker <jwalker@microsoft.com>
2018-02-16 08:23:29 -0500

    Update basic example

--- Examples/Basic_USR_Example.ps1
+++ Examples/Basic_USR_Example.ps1
@@ -4,17 +4,18 @@ Configuration UserRights
 
     Node localhost
     {
-        #Assign shutdown privileges to only Builtin\Administrators
+        # Assign shutdown privileges to only Builtin\Administrators
         UserRightsAssignment AssignShutdownPrivilegesToAdmins
-        {            
-            Policy = "Shut_down_the_system"
+        {
+            Policy   = "Shut_down_the_system"
             Identity = "Builtin\Administrators"
+            Force    = $true
         }
 
-        #Assign access from the network privileges to "contoso\TestUser1" and "contoso\TestUser2"
+        # Assign access from the network privileges to "contoso\TestUser1" and "contoso\TestUser2" without overwritting existing identities
         UserRightsAssignment AccessComputerFromNetwork
         {
-            Policy = "Access_this_computer_from_the_network"
+            Policy   = "Access_this_computer_from_the_network"
             Identity = "contoso\TestUser1","contoso\TestUser2"
         }
     }
